49c7df34d8329623e797daf2e42d001ea090b2a4
Chukwukelunze Chiagozie Melvin <odogwuml01@gmail.com>
2023-08-23 11:57:43 +0100

    Refactor: divided the code into functins and fixed infinite loop

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from art import logo
 import random
 import os
 
-def start_message():
-    to_play = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
-    return to_play
+CARD_LIST = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
+def start_message():
+    return input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
+     
 
 def display_card_info(user_card_list, computer_card_list):
     # display user current cards and current total card value
@@ -14,60 +15,70 @@ def display_card_info(user_card_list, computer_card_list):
     # display dealers current cards and current total card value
     print(f"Computer's first card: {computer_card_list[0]}")
 
-while start_message() == "y":
-    os.system("clear")
-    print(logo)
+def display_final_hands(user_hand, computer_hand):
+    print(f"Your final hand: {user_hand}, final score: {sum(user_hand)}")
+    print(f"Computer's final hand: {computer_hand}, final score: {sum(computer_hand)}")
 
-    # list of cards
-    card_list = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-    bet_price = int(input("How much will you bet?: "))
-    user_card_list = random.sample(card_list, 2)
-    dealer_card_list = random.sample(card_list, 2)
+def ace_card(card, users_hand):
+     if card == 11 and sum(users_hand) < 21:
+        ace_value = int(input("Select ace value (1/11): "))
+        return ace_value
 
-    # display curren information
-    display_card_info(user_card_list, dealer_card_list)
-    
+def Black_jack():
 
-    to_continue = input("Type 'y' to get another card, type 'n' to pass: ").lower()
+    while start_message() == "y":
+        os.system("clear")
+        print(logo)
 
-    while to_continue == "y":
-        # get new card at random from the list of cards
-        new_card = random.choice(card_list)
-        if new_card == 11 and sum(user_card_list) < 21:
-            ace_value = int(input("Select ace value (1/11): "))
-            new_card = ace_value
-        # Add the new card to the users list of cards
-        user_card_list.append(new_card)
-        display_card_info(user_card_list, dealer_card_list)
+        # list of cards
+        bet = int(input("How much will you bet?: "))
+        users_hand = random.sample(CARD_LIST, 2)
+        dealers_hand = random.sample(CARD_LIST, 2)
+
+        # display curren information
+        display_card_info(users_hand, dealers_hand)
         
 
-        if sum(user_card_list) > 21:
-            print(f"your final hand: {user_card_list}, final score: {sum(user_card_list)}")
-            print(f"Computers final hand: {dealer_card_list}, final_score: {sum(dealer_card_list)}")
-            print("You went over. You lose")
-        elif user_card_list == 21:
-            print(f"your final hand: {user_card_list}, final score: {sum(user_card_list)}")
-            print(f"Computers final hand: {dealer_card_list}, final_score: {sum(dealer_card_list)}")
-            print("You win!")
+        to_continue = input("Type 'y' to get another card, type 'n' to pass: ").lower()
+
+        while to_continue == "y":
+            # get new card at random from the list of cards
+            new_card = random.choice(CARD_LIST)
+            new_card = ace_card(new_card)
+
+            # Add the new card to the users list of cards
+            users_hand.append(new_card)
+
+            display_card_info(users_hand, dealers_hand)
+            
+
+            if sum(users_hand) > 21:
+                display_final_hands(users_hand, dealers_hand)
+                return "You went over. You lose"
+            elif sum(users_hand) == 21:
+                display_final_hands(users_hand, dealers_hand)
+                return "You win!"
+            else:
+                to_continue = input("Type 'y' to get another card, type 'n' to pass: ").lower()
+
+
+        user_total_card_value = sum(users_hand)
+        dealers_total_card_value = sum(dealers_hand)
+
+        if dealers_total_card_value < 17:
+            additonal_dealer_card = random.choice(CARD_LIST)
+            dealers_hand.append(additonal_dealer_card)
+
+        if user_total_card_value > dealers_total_card_value:
+                display_final_hands(users_hand, dealers_hand)
+                return "You win!"
+        elif dealers_total_card_value > user_total_card_value:
+                display_final_hands(users_hand, dealers_hand)
+                return "Computer wins!"
         else:
-            to_continue = input("Type 'y' to get another card, type 'n' to pass: ").lower()
+             return "It's a tie"
+    
 
 
-    user_total_card = sum(user_card_list)
-    dealers_total_card = sum(dealer_card_list)
-
-    if dealer_card_list < 17:
-        additonal_card = random.choice(card_list)
-        dealer_card_list.append(additonal_card)
-
-    if user_total_card > dealers_total_card:
-            print(f"your final hand: {user_card_list}, final score: {user_total_card}")
-            print(f"Computers final hand: {dealer_card_list}, final_score: {dealers_total_card}")
-            print("You win!")
-    elif dealers_total_card > user_total_card:
-            print(f"your final hand: {user_card_list}, final score: {user_total_card}")
-            print(f"Computers final hand: {dealer_card_list}, final_score: {dealers_total_card}")
-            print("Computer wins!")
-
-
-
+result = Black_jack()
+print(result)
